79b89b602da343825bfffef4f550ef7517a48d21
Tyler Palesano <supersnapple@gmail.com>
2015-05-16 12:43:35 -0500

    google

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ app.set('port', (process.env.PORT || 5000));
 //});
 
 app.get('/', function(request, response) {
-  var result = ''
+  var result = "<HTML><HEAD><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-63019279-1', 'auto');
+  ga('send', 'pageview');
+
+</script></HEAD><BODY>";
   var times = process.env.TIMES || 5
   for (i=0; i < times; i++) {
 	  result += "<marquee behavior='alternate' direction=" 
@@ -18,6 +27,7 @@ app.get('/', function(request, response) {
 		+ "scrollamount='" + Math.floor(((Math.random() * 12) + 1 ))+ "'"
 		+ ">" + cool() + "</marquee><br />";
   }
+  result += '</BODY></HTML>'
   response.send(result);
 })
 
