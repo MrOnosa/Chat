b9c5543c451f28c8fe42426d44d8b6135171d13a
Tyler Palesano <supersnapple@gmail.com>
2015-05-16 10:43:23 -0500

    Demo

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var pg = require('pg');
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
@@ -12,9 +13,21 @@ app.get('/', function(request, response) {
   var result = ''
   var times = process.env.TIMES || 5
   for (i=0; i < times; i++)
-    result += cool() + "<br />";
+    result += "<marquee>"+ cool() + "</marquee><br />";
   response.send(result);
-});
+})
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running on port:" + app.get('port'))
