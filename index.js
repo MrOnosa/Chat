de3590b5320b2ac95ee00c4b925b0c633fdafaac
Tyler Palesano <supersnapple@gmail.com>
2015-05-16 09:57:40 -0500

    Demo

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', function(request, response) {
   response.send(cool());
 });
 
+app.get('/', function(request, response) {
+  var result = ''
+  var times = process.env.TIMES || 5
+  for (i=0; i < times; i++)
+    result += cool();
+  response.send(result);
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running on port:" + app.get('port'))
 })
