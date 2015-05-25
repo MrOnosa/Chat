fea4da3b2a4c47199f7cc1c647ce6e6b94325ff3
Tyler Palesano <supersnapple@gmail.com>
2015-05-24 21:15:13 -0500

    Chat

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ io.on('connection', function(socket){
 
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(app.get('port'), function(){
+  console.log('listening on *:'+ app.get('port'))
 });
 
 /*app.listen(app.get('port'), function() {
